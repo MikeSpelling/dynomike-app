b76ab55c73fa656e4e20116b370ded8e7aa75b91
Mike <mike.spelling@hotmail.co.uk>
2012-07-11 10:26:26 +0100

    Adding settings for enigma and edited to reuse single view

--- app/controllers/enigma_controller.rb
+++ app/controllers/enigma_controller.rb
@@ -3,8 +3,13 @@ class EnigmaController < ApplicationController
   end
 
   def ciphered
-    input = params[:post][:input_text]
-    enigma = EnigmaMachine.new([1,2,3], ["M", "C", "K"], 1, "")
-    @output = enigma.cipher(input)
+    rotor_numbers = params[:post][:rotor_numbers].split("").map{|num|num.to_i}
+    rotor_offsets = params[:post][:rotor_offsets].split("")
+    reflector_number = params[:post][:reflector_number].to_i
+    plugboard = params[:post][:plugboard]
+    input_text = params[:post][:input_text]
+    enigma = EnigmaMachine.new(rotor_numbers, rotor_offsets, reflector_number, plugboard)
+    @output = enigma.cipher(input_text)
+    render :action => 'index'
   end
 end
