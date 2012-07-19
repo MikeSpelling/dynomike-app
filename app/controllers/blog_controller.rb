496b3b411bbee1c58a5f191c34369df2a5dd6f07
Mike <mike.spelling@hotmail.co.uk>
2012-07-19 12:00:17 +0100

    Adding authentication

--- app/controllers/blog_controller.rb
+++ app/controllers/blog_controller.rb
@@ -1,16 +1,21 @@
 class BlogController < ApplicationController
+
+  before_filter :authenticate, :except => [:index]
+
+  def authenticate
+    authenticate_or_request_with_http_basic do |username, password|
+      username == "admin" && password == Password.all[0][:text]
+    end
+  end
+
   def index
     @blogs = Blog.all
   end
 
   def post
-    password = params[:post][:password]
-    expected_password = Password.all[0][:text]
-    if password == expected_password
-      blog_post = {:title => params[:post][:title], :text => params[:post][:text]}
-      @blog = Blog.new(blog_post)
-      @blog.save
-    end
+    blog_post = {:title => params[:post][:title], :text => params[:post][:text]}
+    @blog = Blog.new(blog_post)
+    @blog.save
     @blogs = Blog.all
 
     render :action => 'index'
