c8352d010e54f3170cce3cc27dea6e8f7bc2ace2
Mike <mike.spelling@hotmail.co.uk>
2012-07-29 18:59:03 +0100

    Fixing random enigma settings

--- app/controllers/enigma_controller.rb
+++ app/controllers/enigma_controller.rb
@@ -29,7 +29,7 @@ class EnigmaController < ApplicationController
   end
 
   def random
-    num_rotors = rand(0..7)
+    num_rotors = rand(1..8)
     rotor_numbers = Array(1..8).shuffle[0...num_rotors].join
     rotor_offsets = (0...num_rotors).map { ("A".."Z").to_a.shuffle[0] }.join
     reflector_number = rand(1..5)
