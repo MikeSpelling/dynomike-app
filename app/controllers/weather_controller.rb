558a204a6af9e9ae20d7868d3f296a9b5fd8e611
Mike <mike.spelling@hotmail.co.uk>
2012-08-08 15:15:31 +0100

    Editing view of weather

--- app/controllers/weather_controller.rb
+++ app/controllers/weather_controller.rb
@@ -4,7 +4,7 @@ class WeatherController < ApplicationController
     @ip = request.remote_ip
     weather_xml = `curl -s "http://free.worldweatheronline.com/feed/weather.ashx?key=9d38708210104504120808&includeLocation=yes&q=#{@ip}"`
 
-    @location = `echo "#{weather_xml}" | grep -o "<areaName>.*</areaName>" | cut -c 20- | sed 's/...............$//'`
+    @location = `echo "#{weather_xml}" | grep -o "<areaName>.*</areaName>" | cut -c 20- | sed 's/..............$//'`
     @temp = `echo "#{weather_xml}" | grep -o "<temp_C>.*</temp_C>" | cut -c 9- | sed 's/.........$//'`
     @windSpeed = `echo "#{weather_xml}" | grep -o "<current_condition>.*</current_condition>" | grep -o "<windspeedMiles>.*</windspeedMiles>" | cut -c 17- | sed 's/.................$//'`
     @windDirection = `echo "#{weather_xml}" | grep -o "<current_condition>.*</current_condition>" | grep -o "<winddir16Point>.*</winddir16Point>" | cut -c 18- | sed 's/..................$//'`
