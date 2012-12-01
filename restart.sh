17a49f7290fc1c7d9d6fe8e4ef224b6dc8417edb
Mike <mike.spelling@hotmail.co.uk>
2012-12-01 11:26:41 +0100

    Adding files I thought were already there???

---
+++ restart.sh
@@ -0,0 +1,5 @@
+./../bin/restart
+rm -f public/index.html
+rm -f public/balls.html
+rm -f public/blog.html
+rm -rf public/blog
---
+++ update.sh
@@ -0,0 +1,6 @@
+git pull
+./../bin/restart
+rm -f public/index.html
+rm -f public/balls.html
+rm -f public/blog.html
+rm -rf public/blog
